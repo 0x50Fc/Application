b2c154c0c09b97be81eedcdc6233613c3c86e011
zhang hailong <hailongz@qq.com>
2015-09-09 10:39:00 +0800

    add demo

---
+++ ios/Application/AppDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppDemo
+//
+//  Created by ZhangHailong on 15/9/9.
+//  Copyright (c) 2015年 hailong.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) IBOutlet UIWindow *window;
+
+
+@end
+
